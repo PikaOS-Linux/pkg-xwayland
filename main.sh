0d742002e7db72c2ff9761b8edc3c0bbe741cdbf
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-25 14:21:52 +0300

    whoops... sorry i was using outdated configs

---
+++ main.sh
@@ -0,0 +1,22 @@
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://gitlab.freedesktop.org/xorg/xserver.git -b xwayland-23.0.99.901
+cp -rvf ./debian ./xserver
+mv ./xserver ./xwayland
+cd ./xwayland
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dh_make --createorig
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
