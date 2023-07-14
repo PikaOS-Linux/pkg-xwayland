ff26feb0305001136af7f6e979bdf61c44d7b0aa
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-07-14 19:43:36 +0300

    Prepare for new egl wayland

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 # Clone Upstream
-git clone https://gitlab.freedesktop.org/xorg/xserver.git -b xwayland-23.1.2
-cp -rvf ./debian ./xserver
-mv ./xserver ./xwayland
+git clone https://gitlab.freedesktop.org/xorg/xserver.git ./xwayland -b xwayland-23.1.2
+cp -rvf ./debian ./xwayland/
 cd ./xwayland
 
 # Get build deps
@@ -9,7 +8,7 @@ apt-get build-dep ./ -y
 
 # Build package
 LOGNAME=root dh_make --createorig -y -l -p xwayland_23.1.2
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
