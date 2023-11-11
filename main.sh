68da656c6290ac8ecb4cd0c330b507cfd868dd99
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-11-11 17:18:49 +0300

    23.2.2 + explict sync for nvidia patch

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 # Clone Upstream
-git clone https://gitlab.freedesktop.org/xorg/xserver.git ./xwayland -b xwayland-23.2.1
+git clone https://gitlab.freedesktop.org/xorg/xserver.git ./xwayland -b xwayland-23.2.2
 cp -rvf ./debian ./xwayland/
 cd ./xwayland
 
@@ -8,7 +8,7 @@ apt upgrade -y
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p xwayland_23.2.1
+LOGNAME=root dh_make --createorig -y -l -p xwayland_23.2.2
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
