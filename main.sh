373cf7faa4873ecd8de860a1971fe0ca120f2ed1
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-28 20:30:11 +0300

    bot

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 # Clone Upstream
-git clone https://gitlab.freedesktop.org/xorg/xserver.git -b xwayland-23.2.3 ./xwayland
+git clone https://gitlab.freedesktop.org/xorg/xserver.git -b xwayland-23.2.4 ./xwayland
 cp -rvf ./debian ./xwayland/
+cp -vf ./meson.build ./xwayland/
 cd ./xwayland
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
